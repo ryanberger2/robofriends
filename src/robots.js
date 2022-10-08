9096cbb85535fc9131171dfbdfa1cc27cb98b333
Ryan Berger <rberger@etsy.com>
2022-10-08 15:42:00 -0400

    initial robofriends push

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+    {
+        id: 1, 
+        name: 'Amanda Dinan', 
+        username: 'Amandaaa', 
+        email: 'amanda.dinan@gmail.com'
+    }, 
+    {
+        id: 2, 
+        name: 'Kim Dinan', 
+        username: 'Kim', 
+        email: 'kim.dinan@gmail.com'
+    }, 
+    {
+        id: 3, 
+        name: 'Brian Patton', 
+        username: 'Brian', 
+        email: 'brian.patton@gmail.com'
+    }, 
+    {
+        id: 4, 
+        name: 'Juniper Patton', 
+        username: 'Junie', 
+        email: 'kid@kidmail.com'
+    }, 
+    {
+        id: 5, 
+        name: 'Arlo Patton', 
+        username: 'Arlo', 
+        email: 'littlebrother@kidmail.com'
+    }, 
+    {
+        id: 6, 
+        name: 'Mona Lisa Dinan', 
+        username: 'Mona', 
+        email: 'dog@petmail.com'
+    }, 
+    {
+        id: 7, 
+        name: 'Marlon Dinan', 
+        username: 'Marlon', 
+        email: 'cat@petmail.com'
+    }, 
+    {
+        id: 8, 
+        name: 'Ryan Berger', 
+        username: 'Ryan', 
+        email: 'berger82@gmail.com'
+    }, 
+    {
+        id: 9, 
+        name: 'Janie Hollowell', 
+        username: 'Janie', 
+        email: 'janiedances@gmail.com'
+    }, 
+    {
+        id: 10, 
+        name: 'John Forester', 
+        username: 'John', 
+        email: 'jforester@etsy.com'
+    }, 
+    {
+        id: 11, 
+        name: 'Cheryl Nana', 
+        username: 'Cheryl', 
+        email: 'cheryl@grandmamail.com'
+    } 
+]
